7e96abe436dbd8af4cd398f1cb0342a306fa32c7
Mvke-IT <michael.duarte09@gmail.com>
2024-01-23 13:07:54 -0400

    deleted new line

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -18,4 +18,3 @@ elif number > 0:
         print(str(number)[-1:], 'and is 0')
     elif number % 10 < 6 and number % 10 != 0:
         print(str(number)[-1:], 'and is less than 6 and not 0')
-        
