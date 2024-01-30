143bddb4c8d107a03deecdf3b85f1f4d2a713a30
Mvke-IT <michael.duarte09@gmail.com>
2024-01-30 12:25:18 -0400

    Wrote a function that computes the square value of all integers of a matrix.

--- python-more_date_structures/0-square_matrix_simple.py
+++ python-more_date_structures/0-square_matrix_simple.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new matrix = []
+    for m in matrix:
+        new_matrix.appened(lis(map(lambda x: pow(x, 2), m )))
+        return new_matrix
