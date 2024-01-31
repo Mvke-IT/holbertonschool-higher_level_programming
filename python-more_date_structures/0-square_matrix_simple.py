0133fbfca794702b5da5a26888dcdd2f1713f6f5
Mvke-IT <michael.duarte09@gmail.com>
2024-01-31 10:06:11 -0400

    updating

--- python-more_date_structures/0-square_matrix_simple.py
+++ python-more_date_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new matrix = []
+    new_matrix = []
     for m in matrix:
         new_matrix.appened(lis(map(lambda x: pow(x, 2), m )))
         return new_matrix
