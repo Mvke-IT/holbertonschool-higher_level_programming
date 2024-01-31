6e00b9f9f359427fb84a7de33474753d80d4f050
Mvke-IT <michael.duarte09@gmail.com>
2024-01-31 10:15:53 -0400

    fixed typo

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for m in matrix:
-        new_matrix.appened(lis(map(lambda x: pow(x, 2), m )))
+        new_matrix.appened(list(map(lambda x: pow(x, 2), m )))
         return new_matrix
