eb2ff0d3942607647fc9164a6159f4ee19546d59
Mvke-IT <michael.duarte09@gmail.com>
2024-01-31 13:54:58 -0400

    Wrote a function that prints a disctionary by ordered keys.

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_dic = dict(sorted(a_dictionary.items()))
+    for elem in sorted(sorted_dic.items()):
+        print(elem[0] + ":", elem[1])
