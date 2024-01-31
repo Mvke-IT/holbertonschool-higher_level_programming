75c5e302987e58836062d53e41d0d5d9209768f0
Mvke-IT <michael.duarte09@gmail.com>
2024-01-31 10:18:14 -0400

    fixed syntax again

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    for m in matrix:
-        new_matrix.appened(list(map(lambda x: pow(x, 2), m )))
-        return new_matrix
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: pow(x, 2), i)))
+    return(new_matrix)
