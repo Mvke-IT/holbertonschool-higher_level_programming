4e12db9330f1597e4d3a2724ea85fc4ba0671b46
Mvke-IT <michael.duarte09@gmail.com>
2024-02-09 14:21:28 -0400

    Wrote a function that reads a text file (UTF8) and prints it to stdout:

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This module has a function called read_file,
+that reads and prints a file that it have been past
+"""
+
+
+def read_file(filename=""):
+    """
+    This function that read a text file
+    and prints it to stdout
+    """
+    with open(filename, 'r') as fd:
+        for line in fd:
+            print(line, end="")
