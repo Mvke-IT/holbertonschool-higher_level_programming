c844fd61f254866da233bae4c5bb4bf5079b4cd9
Mvke-IT <michael.duarte09@gmail.com>
2024-02-09 14:01:08 -0400

    wrote a class that's inherited

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+    class that in inherits from list
+"""
+
+
+class MyList(list):
+    """
+        method to sort a list
+    """
+    def print_sorted(self):
+        print(sorted(self))
