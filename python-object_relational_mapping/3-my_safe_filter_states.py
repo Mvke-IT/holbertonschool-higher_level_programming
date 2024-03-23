00e53be5791ac020ffbc4fbd0eb8b78c05a122e8
Mvke-IT <michael.duarte09@gmail.com>
2024-03-22 23:25:51 -0400

    fixed port number to 3304

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ import MySQLdb as sql
 from sys import argv
 if __name__ == '__main__':
     db = sql.connect(host="localhost",
-                     port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+                     port=3304, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     query = "SELECT * FROM states WHERE name LIKE BINARY %s"
     cur.execute(query, (argv[4],))
