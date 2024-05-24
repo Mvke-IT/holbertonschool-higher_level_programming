9b5fcaacfdb8b3f07f5fc012e3b96ffbe417897f
Mvke-IT <michael.duarte09@gmail.com>
2024-05-24 09:55:54 -0400

    accidentaly had uploaded main file instead of this script

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Holberton School</title>
-  </head>
-  <body>
-    <header>
-      First HTML page
-    </header>
-    <div id="red_header">Red header</div>
-    <footer>
-      Holberton School - 2022
-    </footer>
-    <script type="text/javascript" src="1-script.js"></script>
-  </body>
-</html>
+const redHeader = document.getElementById('red_header');
+redHeader.addEventListener('click', function() {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
